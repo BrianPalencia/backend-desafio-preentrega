7f5eb83e0e64a9c1c1ab251857cbae007a1a8b3f
Brian Palencia <briandonaldo@gmail.com>
2023-12-05 17:06:37 -0600

    desafio 3

---
+++ src/manager.js
@@ -0,0 +1,28 @@
+import { ProductManager } from "./ProductManager.js";
+
+let miPrimeraTienda = new ProductManager("./products.json");
+
+miPrimeraTienda.addProduct(
+    "Cereal",
+    "Cereal de arroz",
+    200,
+    "sin imagen",
+    "001",
+    10
+);
+miPrimeraTienda.addProduct(
+    "Mantequilla de mani",
+    "Mantequilla de mani",
+    200,
+    "sin imagen",
+    "002",
+    10
+);
+miPrimeraTienda.addProduct(
+    "Aceite de girasol", 
+    "Aceite de girasol",
+    200,
+    "sin imagen",
+    "003",
+    10
+);
