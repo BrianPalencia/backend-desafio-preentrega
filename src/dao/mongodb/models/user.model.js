b94b8a786aea836280ea8412b2381d2293c2c4af
Brian Palencia <briandonaldo@gmail.com>
2024-02-21 11:07:11 -0600

    avance del desafio, aun hay cosas por hacer

--- src/dao/mongodb/models/user.model.js
+++ src/dao/mongodb/models/user.model.js
@@ -1,16 +1,45 @@
 import mongoose from "mongoose";
 
-const userCollection = "users";
+const userCollection = "Users";
 
-const UserSchema = new mongoose.Schema({
-    first_name: { type: String, require: true, max: 100 },
-    last_name: { type: String, require: true, max: 100 },
-    email: { type: String, require: true, max: 100 },
-    password: { type: String, require: true, max: 100 },
-    age: { type: Number, require: true, max: 100 },
-    role: { type: String, require: true, max: 100 },
+const usersSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    dni: Number,
+    birthDate: Date,
+    password: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum: ["M", "F"],
+    },
+    role: {
+        type: String,
+        enum: ["student", "teacher"],
+        default: "student",
+    },
+    courses: {
+        type: [
+            {
+                type: mongoose.SchemaTypes.ObjectId,
+                ref: "Courses",
+            },
+        ],
+        default: [],
+    },
 });
 
-const User = mongoose.model(userCollection, UserSchema);
-
-export default User;
+export const userModel = mongoose.model(userCollection, usersSchema);
